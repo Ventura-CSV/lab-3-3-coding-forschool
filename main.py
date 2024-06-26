e84b0ad1713ce66c329cc65f090e3d639fe59067
Ethan Wang <ethan_wang2@my.vcccd.edu>
2024-06-26 13:35:42 -0700

    first

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ def main():
     num1 = int(input('Enter the first number: '))
     num2 = int(input('Enter the second number: '))
     num3 = int(input('Enter the third number: '))
-    """
-    Code Your Program here
-    """
+
+    maxnum = 0
+    if num1 > num2 and num1 > num3:
+        maxnum =num1
+    elif num2 > num1 and num2 > num3:
+        maxnum = num2
+    elif num3 > num1 and num3 > num2:
+        maxnum = num3       
 
     print(f'The greates number is {maxnum}')
     ########################################
